82a1cd481cf499496b2de8ed3271b9dc84c53768
sandip chakraborty <unicornsc77@gmail.com>
2026-06-27 09:09:01 +0530

    change transport port 587 and secure:false(again)

--- Router/userRoute.js
+++ Router/userRoute.js
@@ -21,7 +21,7 @@ function generateOTP() {
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
